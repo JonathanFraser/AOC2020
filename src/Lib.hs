78895d3ba00b2b99e729ca15d75c9ad4d0385162
Jonathan Fraser <jonathanfras@gmail.com>
2020-12-01 22:08:59 -0800

    added day2

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,14 @@
 module Lib
     ( 
         day1part1,
-        day1part2
+        day1part2,
+        day2
     ) where
 
 import Control.Monad
-
+import qualified Text.Parsec.Numbers as PN 
+import qualified Text.Parsec.Char as PC
+import qualified Text.Parsec as PT
 
 matchPair :: [a] -> ((a,a) -> Bool) -> [(a,a)]
 matchPair xs chk = do 
@@ -19,7 +22,9 @@ readInts fileName = do
         content <- readFile fileName
         let splitLines = lines content 
         let values = map read splitLines :: [Int]
-        return values 
+        return values
+
+
 
 day1part1 :: IO ()
 day1part1 = do
@@ -37,3 +42,57 @@ day1part2 = do
                     guard (a+b+c == 2020)
                     return (a*b*c)
             putStrLn $ show $ r
+
+
+passwordParse = do 
+                start <- PN.parseIntegral  
+                PC.char '-'
+                end <- PN.parseIntegral  
+                PC.space
+                char <- PC.letter 
+                PC.char ':'
+                PC.space 
+                rest <- PT.many PC.alphaNum
+                return (start,end,char,rest)
+
+
+part1rule (s,e,c,str) = let 
+                            count = length $ filter (==c) str 
+                            in 
+                                if count < s then 
+                                     False
+                                else 
+                                    if count > e then 
+                                        False 
+                                    else 
+                                        True
+
+part2rule (i1,i2,c,str) = let 
+                                pos1 = c == (str !! (i1-1))
+                                pos2 = c == (str !! (i2-1))
+                            in 
+                                if pos1 && pos2 then 
+                                    False 
+                                else 
+                                    if pos1 || pos2 then 
+                                        True 
+                                    else 
+                                        False 
+
+
+vetPassword :: MonadFail m => ((Int,Int,Char,String) -> Bool) -> String -> m Bool 
+vetPassword f line = let 
+                    out = PT.parse passwordParse "" line
+                    in 
+                        case out of 
+                            Left r ->
+                                fail $ show r 
+                            Right v -> 
+                                return $ f v
+day2 :: IO ()
+day2 = do
+                values <- lines <$> readFile "inputs/inputd2p1.txt"
+                part1 <- sequence $ fmap (vetPassword part1rule) values
+                part2 <- sequence $ fmap (vetPassword part2rule) values 
+                print  $ length $ filter id part1
+                print $ length $ filter id part2 
